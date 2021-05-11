2e6d15706495dd06ff3632869b6203f028837ea8
rain1976 <49852203+rain1976@users.noreply.github.com>
2021-05-11 10:30:58 +0900

    Touch 사운드 추가. 화면을 터지해주세요 문구 추가.

---
+++ LGUPlus_v2/Assets/Scripts/SoundOneShotPlay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOneShotPlay : MonoBehaviour
+{
+    public AudioClip audioClip;
+    public AudioSource audioSources;
+
+    private void Awake()
+    {
+        audioSources.Play();
+        StartCoroutine(SoundPlayTime(60f));
+    }
+
+    IEnumerator SoundPlayTime(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        audioSources.Play();
+        StartCoroutine(SoundPlayTime(60f));
+    }
+}
